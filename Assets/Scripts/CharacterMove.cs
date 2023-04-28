f83ef7062fd962980583608fe6cd96d606c94e88
Theo-Rokas <t.rokas@hotmail.com>
2023-04-28 21:00:29 +0300

    Fix CammeraFollow script and add stars to the scene

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -24,7 +24,7 @@ public class CharacterMove : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
